4b3eb81a07c8bb264a03361f3df392eb086917d0
andrey.isaev <an.isaev7@gmail.com>
2022-06-30 10:56:09 +0300

    complete difficult part

--- HW3.py
+++ HW3.py
@@ -1,19 +1,26 @@
 questions = ["My name ___ Vova", "I ___ a coder", "I live ___ Moscow"]
 answers = ["is", "am", "in"]
 right_answers = 0
+score = 0
 
 print('Привет! Предлагаю проверить свои знания английского! Наберите "ready", чтобы начать!')
 is_ready = input()
 if is_ready == 'ready':
     for i in range(len(questions)):
-        print(questions[i])
-        user_answer = input()
-        if user_answer == answers[i]:
-            print('Ответ верный!')
-            right_answers += 1
-        else:
-            print(f'Неправильно. Правильный ответ: {answers[i]}')
-    right_answers_percent = right_answers / len(questions) * 100
-    print(f'Вот и все! Вы ответили на {right_answers} вопросов из {len(questions)} верно, это {round(right_answers_percent)} процентов.')
+        for attempt in range(2, -1, -1):
+            print(questions[i])
+            user_answer = input()
+            if user_answer == answers[i]:
+                print('Ответ верный!')
+                right_answers += 1
+                score += attempt + 1
+                break
+            else:
+                if attempt == 0:
+                    print(f'Увы, но нет. Верный ответ: {answers[i]}')
+                else:
+                    print(f'Осталось попыток: {attempt}, попробуйте еще раз!')
+
+    print(f'Вот и все! Вы ответили на {right_answers} вопросов из {len(questions)} верно, вы набрали {score} баллов.')
 else:
     print('Кажется, вы не хотите играть. Очень жаль.')
