d80ef17ab671301bc61753b59aa602802f29af2f
andrey.isaev <an.isaev7@gmail.com>
2022-07-04 00:04:14 +0300

    add ending foo

--- HW3_easy_part.py
+++ HW3_easy_part.py
@@ -1,17 +1,19 @@
-#обьявляем переменные
+from sklonenya import end_of_words
+
+# объявляем переменные
 questions = ["My name ___ Vova", "I ___ a coder", "I live ___ Moscow"]
 answers = ["is", "am", "in"]
 right_answers = 0
 
-#Вывод приветствия и условий для начала игры
+# Вывод приветствия и условий для начала игры
 print('Привет! Предлагаю проверить свои знания английского! Наберите "ready", чтобы начать!')
 is_ready = input()
 
-#Проверка условия на запуск игры
+# Проверка условия на запуск игры
 if is_ready == 'ready':
-    #Создадим цикл, который будет перебирать индексы вопросов и ответов
+    # Создадим цикл, который будет перебирать индексы вопросов и ответов
     for i in range(len(questions)):
-        #задаем вопрос и получаем ответ
+        # задаем вопрос и получаем ответ
         print(questions[i])
         user_answer = input()
 
@@ -28,7 +30,10 @@ if is_ready == 'ready':
     right_answers_percent = right_answers / len(questions) * 100
 
     # выводим общий результат
-    print(f'Вот и все! Вы ответили на {right_answers} вопросов из {len(questions)} верно, это {round(right_answers_percent)} процентов.')
+    print_first_part = f'Вы ответили на {right_answers} вопрос{end_of_words(right_answers)} из {len(questions)} верно'
+    print_second_part = f'это {round(right_answers_percent)} процент{end_of_words(round(right_answers_percent))}.'
+
+    print(f'Вот и все! {print_first_part}, {print_second_part}')
 else:
-    #Если пользователь ввел не ready
+    # Если пользователь ввел не ready
     print('Кажется, вы не хотите играть. Очень жаль.')
